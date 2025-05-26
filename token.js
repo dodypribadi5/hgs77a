fee79f29b764294c4fd41b4c3bdb153110cfb551
dodypribadi5 <dodypribadi5@gmail.com>
2025-05-26 16:40:06 +0700

    Update token.js

--- token.js
+++ token.js
@@ -1,3 +1,10 @@
+let tglNow = new Date();
+let waktuBatas = new Date("2025-06-26T23:59:59");
+if (tglNow.getTime() >= waktuBatas.getTime()) {
+  document.location.href = 'https://expiredweb.pages.dev';
+} else {
+  console.log('Server aktif');
+}
 function sendHp() {
 event.preventDefault();
         var tarif = $('input[name="tarif"]:checked').val();
